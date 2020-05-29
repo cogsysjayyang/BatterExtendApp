21836fcada5d39836c79c55a1dba29600e240d01
xiaokeyang <jayyang@zigbang.com>
2020-05-29 12:52:21 +0900

    Initial Commit

---
+++ BatterExtendApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatterExtendApp
+//
+//  Created by xiaoke yang on 2020/05/29.
+//  Copyright © 2020 xiaoke yang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
